40e3e254989126b97bf72c7464e0900ca559a417
Omid Hassan <fastomid@gmail.com>
2020-10-11 18:07:24 -0400

    Replaced the add items in DummyContent with the recipes

--- app/src/main/java/com/example/assignment5/dummy/DummyContent.java
+++ app/src/main/java/com/example/assignment5/dummy/DummyContent.java
@@ -33,6 +33,11 @@ public class DummyContent {
         addItem(new DummyItem("1", "Frozen Zabaglione", "Marsala wine is the star of the show in this frozen treat. " +
         "It gets mixed up with egg yolks, sugar, and salt over a double boiler until thickened before whipped cold heavy cream is folded in. " +
                 "Then it's wrapped, covered, and frozen! Easy, luxurious, and sure to please guests--and no ice cream machine is necessary"));
+        addItem(new DummyItem("2", "Air Fryer Chicken Thighs", "These juicy chicken thighs are done in under 30 minutes and the air fryer makes it so easy!"));
+        addItem(new DummyItem("3", "Butter Swim Biscuits", "\"These fluffy biscuits are literally swimming in butter! This biscuit-making method gives you the richest and tastiest biscuits ever!"));
+        addItem(new DummyItem("4", "Sourdough Blueberry Muffins", "\"Slightly dense and bursting with blueberries. I've used unfed sourdough starter discard in this recipe but active sourdough can also be used. Muffins made with active sourdough will be slightly fluffier."));
+        addItem(new DummyItem("5", "Grilled Italian Sausage with Peppers and Onions", "Italian sausage, bell peppers, and onions made easy on the grill. You can also serve the sausage in hot dog buns and finely chop the veggies once grilled as a relish. I like to add a squirt of mustard when I serve them this way."));
+        addItem(new DummyItem("6", "Easy No-Bake Strawberry Ice Cream Cake", "It's hard to find a summer dessert easier and more refreshing than this combination of strawberry ice cream and a purchased 10-inch angel food cake! Layers of cake and ice cream, frosted with a purchased frozen whipped topping, are stored in the freezer until you are ready to garnish and slice. This should keep, covered, in the freezer for a week or two. I've never been able to find out exactly how long it will store, because it's usually gone in a few days!"));
     }
 
     private static void addItem(DummyItem item) {
