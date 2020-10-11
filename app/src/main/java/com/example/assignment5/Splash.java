a73dac1566ef065b39425e84bc6ea2c77b3a98e3
Omid Hassan <fastomid@gmail.com>
2020-10-11 18:46:30 -0400

    Forgot to add song in the background of the splash screen Song is very quiet so might need to turn up Also the assignment is completed

--- app/src/main/java/com/example/assignment5/Splash.java
+++ app/src/main/java/com/example/assignment5/Splash.java
@@ -1,6 +1,7 @@
 package com.example.assignment5;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,14 +10,20 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Splash extends AppCompatActivity {
+    MediaPlayer song;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        song = new MediaPlayer();
+        song = MediaPlayer.create(this, R.raw.song);
+        song.seekTo(8000);
+        song.start();
         TimerTask task=new TimerTask() {
             @Override
             public void run() {
                 finish();
+                song.pause();
                 startActivity(new Intent(Splash.this, RecipeListActivity.class));
 
             }
